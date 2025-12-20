0f69eae03fdf1834d231315bf1cfdd145e60c4f7
Emanuele Nisticò <emanuelenist@gmail.com>
2025-12-20 18:04:03 +0100

    Creazione di un controller per gli accessi e selezione pagina iniziale sulla base del user

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,12 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'controller/app_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'controller/auth_controller.dart';
+import 'UI/pages/profilo_page.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
@@ -27,14 +27,22 @@ class MainApp extends StatelessWidget {
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
       ],
-      supportedLocales: const [
-        Locale('it'),
-        Locale('en'),
-      ],
+      supportedLocales: const [Locale('it'), Locale('en')],
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       debugShowCheckedModeBanner: false,
-      home: const App(),
+      //Verifica se l'utente ha già effettuato l'accesso e su questo
+      //decide cosa svolgere
+      home: StreamBuilder(
+        stream: authController().authStateChanges,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const App();
+          } else {
+            return ProfiloPage();
+          }
+        },
+      ),
     );
   }
 }
