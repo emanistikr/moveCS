32677dc08a3a1e769f0f71b1183ef9750cc7b5f5
Emanuele Nisticò <emanuelenist@gmail.com>
2025-12-20 21:52:12 +0100

    Traduzioni pagina login_and_registration_page.dart ed inserimento di tasto per nascondere/visualizzare password

--- lib/UI/pages/login_and_registration_page.dart
+++ lib/UI/pages/login_and_registration_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../../config/app_text_styles.dart';
 import '../../controller/auth_controller.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import '../../controller/app_localization.dart';
 
 class LoginAndRegistrationPage extends StatefulWidget {
   const LoginAndRegistrationPage({super.key});
@@ -14,65 +15,115 @@ class _LoginAndRegistrationPageState extends State<LoginAndRegistrationPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool isLogin = true; // true per login, false per registrazione
+  bool _passwordVisible = false;
 
   Future<void> signIn() async {
-    try{
-      await AuthController().signInWithEmailAndPassword(_emailController.text,_passwordController.text);
-    } on FirebaseAuthException catch (error){}
+    try {
+      await AuthController().signInWithEmailAndPassword(
+        _emailController.text,
+        _passwordController.text,
+      );
+    } on FirebaseAuthException catch (error) {}
   }
 
   Future<void> createUser() async {
-    try{
-      await AuthController().createUserWithEmailAndPassword(_emailController.text,_passwordController.text);
-    } on FirebaseAuthException catch (error){}
+    try {
+      await AuthController().createUserWithEmailAndPassword(
+        _emailController.text,
+        _passwordController.text,
+      );
+    } on FirebaseAuthException catch (error) {}
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(isLogin ? 'Login' : 'Registrazione'),
+        title: Text(
+          isLogin
+              ? 'Login'
+              : AppLocalizations.of(context)?.translate("signUp") ?? "",
+        ),
         titleTextStyle: AppTextStyles.title,
       ),
       body: Column(
-          children: [
-            Column(
-              children: [
-                const SizedBox(height: 60),
-                TextField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                    label: Text('Email', style: AppTextStyles.smallTitle),
-                  ),
+        children: [
+          Column(
+            children: [
+              const SizedBox(height: 60),
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  label: Text('Email', style: AppTextStyles.smallTitle),
                 ),
-                TextField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _passwordController,
+                      obscureText: !_passwordVisible,
+                      decoration: const InputDecoration(
+                        label: Text(
+                          'Password',
+                          style: AppTextStyles.smallTitle,
+                        ),
+                      ),
+                    ),
+                  ),
 
-                    label: Text('Password',style: AppTextStyles.smallTitle),
+                  IconButton(
+                    icon: Icon(
+                      _passwordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    },
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 40),
-            Column(
-              children: [
-                ElevatedButton(onPressed: (){
-                  isLogin ? signIn() : createUser();
-                }, child: Text(
-                  isLogin ? 'Accedi' : 'Crea Account',
-                  style: AppTextStyles.smallTitle ,
-                )
-                ),
-                TextButton(
-                  onPressed: () => setState(() => isLogin = !isLogin),
-                  child: Text(isLogin ? 'Crea un nuovo account' : 'Hai già un account? Accedi'),
-                ),
-              ],
-            ),
-          ],
-        ),
+                ],
+              ),
+              const SizedBox(height: 40),
+              Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      isLogin ? signIn() : createUser();
+                    },
+                    child: Text(
+                      isLogin
+                          ? AppLocalizations.of(context)?.translate("signIn") ??
+                                ""
+                          : AppLocalizations.of(
+                                  context,
+                                )?.translate("createAccount") ??
+                                "",
+                      style: AppTextStyles.smallTitle,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () => setState(() => isLogin = !isLogin),
+                    child: Text(
+                      isLogin
+                          ? AppLocalizations.of(
+                                  context,
+                                )?.translate("passToCreateAccount") ??
+                                ""
+                          : AppLocalizations.of(
+                                  context,
+                                )?.translate("passToLogIn") ??
+                                "",
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
